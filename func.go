0e70da4b9899e32b9d4d9f09c8c6cdbaea566d59
tooomcat <hs87843160@hotmail.com>
2021-08-11 21:25:04 +0800

    modify logic

--- func.go
+++ func.go
@@ -1,14 +1,13 @@
 package myTools
 
 func Add(a, b int) int {
-	return a + b
+	return a + b + 1
 }
 
 func Mul(a, b int) int {
-	return a * b
+	return a*b + 1
 }
 
 func Div(a, b int) int {
-	return a / b
+	return a/b + 1
 }
-
