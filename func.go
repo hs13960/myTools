c00d05b8f4c57c25381c0ebdc9274cd1d5c8fa61
tooomcat <hs87843160@hotmail.com>
2021-08-11 21:31:50 +0800

    modify v2

--- func.go
+++ func.go
@@ -1,13 +1,13 @@
 package myTools
 
 func Add(a, b int) int {
-	return a + b + 1
+	return a + b + 2
 }
 
 func Mul(a, b int) int {
-	return a*b + 1
+	return a*b + 2
 }
 
 func Div(a, b int) int {
-	return a/b + 1
+	return a/b + 2
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/hs13960/myTools
+module github.com/hs13960/myTools/v2
 
 go 1.16
